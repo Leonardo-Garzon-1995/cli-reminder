ed7256f768e2298109082d652ff4f4a98ea1118c
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-12-02 05:28:14 +0100

    ADD: function timerByCurrentTime() and README
    trying to use current tiem to set up timer

---
+++ README.md
@@ -0,0 +1,22 @@
+# CLI Reminder
+CLI Reminder is a simple command-line tool that allows users to set and manage reminders directly from the terminal. It supports scheduling reminders for specific dates and times, as well as recurring reminders.
+## Features
+- Set one-time reminbders
+
+## How to Use
+1. Install the CLI Reminder tool by following the installation instructions in the repository.
+2. Open your terminal.  
+3. Use the following command to set a reminder:
+```bash
+npm run cmd <feature> [options]
+```
+
+### Examples
+- Set a one-time reminder:
+```bash
+npm run cmd remindme <reminder text> <time> <unit>
+```
+-view current tieme
+```bash
+npm run cmd time
+```
--- bin/main.js
+++ bin/main.js
@@ -1,10 +1,11 @@
 const sound = require("play-sound") ()
 const {displayBanner,} = require("../lib/format");
-const {parseUnit, timeToMillis} = require("../lib/helpers")
+const {parseUnit, timeToMillis, getCurrentTime} = require("../lib/helpers")
 
-const [, , text, num, unit] = process.argv;
+const [, , command, text, num, unit] = process.argv;
 
-function remindme(text) {
+// Set a timer
+function remindme() {
     if (process.argv.length < 5) throw new Error("Invalid parameters");
 
     setTimeout(() => {
@@ -13,5 +14,21 @@ function remindme(text) {
     }, timeToMillis(num, parseUnit(unit)));
 }
 
-remindme(text);
 
+// view Currentime
+function viewTime() {
+    displayBanner(getCurrentTime());
+}
+
+// executing functions
+function execute() {
+
+    if (command === "remindme") {
+        remindme();
+    } else {
+        viewTime();
+    }
+    
+}
+
+execute();
